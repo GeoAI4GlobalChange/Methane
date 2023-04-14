4bdc549600cee6ed2bf9258b74a3f49f574ff707
Fa Li <lifa.lbnl@gmail.com>
2023-04-14 01:55:04 -0500

    Add files via upload

---
+++ BorealArctic/Analysis/methane_analysis_demo.py
@@ -0,0 +1,103 @@
+from netCDF4 import Dataset
+import copy
+from methane_analysis_toolbox import FCH4_anomaly_calculation,Forcing_anomaly_calculation,Par_cor,LinearReg_model,ForcingData_load,FCH4Data_load,FCH4_anomaly_yearly,Forcing_anomaly_yearly
+
+def grid_variation_analysis(max_lat_idx,dir_forcing,dir_fch4,save_file,forcing_file_list,forcing_vars,target_var):
+    #load FCH4 upscaled data (start from the year of 2002)
+    fch4_var,lats,lons=FCH4Data_load(dir_fch4,max_lat_idx)
+    #obtain FCH4 anomaly
+    fch4_var_anomaly=FCH4_anomaly_calculation(fch4_var,lats,lons)
+    #obatain datsets of input forcing (TS, TA, SC, PA, P, SWC, WS, and GPP)
+    all_forcing_vars=ForcingData_load(forcing_file_list,forcing_vars,dir_forcing,max_lat_idx,lats,lons)
+    #obtain forcing anomaly
+    all_forcing_vars_anomaly=Forcing_anomaly_calculation(all_forcing_vars,lats,lons)
+
+    #update forcing_var and all_vars
+    forcing_vars.extend(['gpp'])
+    all_vars=copy.deepcopy(forcing_vars)
+    all_vars.extend(['fch4'])
+
+    #calculate partial correlation between FCH4 and its drivers in each grid
+    partial_result=Par_cor(forcing_vars,lats,lons,all_forcing_vars_anomaly,fch4_var_anomaly,all_vars,target_var)
+
+    #obtain and save the results
+    nc_fid2 = Dataset(save_file, 'w', format="NETCDF4")
+    nc_fid2.createDimension('latitude', len(lats))
+    nc_fid2.createDimension('longitude', len(lons))
+    nc_fid2.createDimension('forcing_id', partial_result.shape[0])
+    latitudes = nc_fid2.createVariable('latitude', 'f4', ('latitude',))
+    longitudes = nc_fid2.createVariable('longitude', 'f4', ('longitude',))
+    forcing_var_id = nc_fid2.createVariable("forcing_id", "f4", ("forcing_id",))
+    par_cor_results = nc_fid2.createVariable('par_r', "f4", ("forcing_id","latitude", "longitude",), zlib=True)
+    forcing_var_id[:] = range(partial_result.shape[0])
+    forcing_var_id.units='_'.join(forcing_vars)
+    latitudes[:] = lats[:]
+    longitudes[:] = lons[:]
+    par_cor_results[:] = partial_result[:]
+    nc_fid2.close()
+
+
+def grid_trend_analysis(forcing_group,max_lat_idx,dir_forcing,dir_fch4,forcing_file_list,forcing_vars):
+    # load FCH4 upscaled data
+    fch4_var, lats, lons = FCH4Data_load(dir_fch4, max_lat_idx)
+    #calculate yearly FCH4 anomaly
+    fch4_var_anomaly = FCH4_anomaly_yearly(fch4_var, lats, lons)
+    # obatain datsets of input forcing (TS, TA, SC, PA, P, SWC, WS, and GPP)
+    all_forcing_vars = ForcingData_load(forcing_file_list, forcing_vars, dir_forcing, max_lat_idx, lats, lons)
+    # obtain forcing anomaly
+    all_forcing_vars_anomaly = Forcing_anomaly_yearly(all_forcing_vars, lats, lons)
+
+    #update forcing_vars and all_vars
+    forcing_vars.extend(['gpp'])
+    all_vars = copy.deepcopy(forcing_vars)
+    all_vars.extend(['fch4'])
+
+    #Quantify the controls from each driver on the trend of FCH4 using a statistical linear regression model
+    corr_result,prediction_result=LinearReg_model(forcing_vars,lats,lons,fch4_var_anomaly,all_forcing_vars_anomaly,all_vars,forcing_group)
+
+    #obtain and save the results
+    save_file = f'./results/FCH4_trend_explanation_{forcing_group}.nc'  # path to save the results
+    nc_fid2 = Dataset(save_file, 'w', format="NETCDF4")
+    nc_fid2.createDimension('latitude', len(lats))
+    nc_fid2.createDimension('longitude', len(lons))
+    nc_fid2.createDimension('forcing_id', corr_result.shape[0])
+    nc_fid2.createDimension('time', prediction_result.shape[0])
+    latitudes = nc_fid2.createVariable('latitude', 'f4', ('latitude',))
+    longitudes = nc_fid2.createVariable('longitude', 'f4', ('longitude',))
+    wetland_type_var = nc_fid2.createVariable("forcing_id", "f4", ("forcing_id",))
+    time_var = nc_fid2.createVariable("time", "f4", ("time",))
+    corr_var = nc_fid2.createVariable('corr', "f4", ("forcing_id", "latitude", "longitude",), zlib=True)
+    Prediction_var = nc_fid2.createVariable('Prediction', "f4", ("time", "latitude", "longitude",), zlib=True)
+    wetland_type_var[:] = range(corr_result.shape[0])
+    wetland_type_var.units = '_'.join(forcing_vars)
+    latitudes[:] = lats[:]
+    longitudes[:] = lons[:]
+    corr_var[:] = corr_result[:]
+    time_var[:]=range(prediction_result.shape[0])
+    Prediction_var[:]=prediction_result[:]
+    nc_fid2.close()
+
+if __name__ == '__main__':
+    max_lat_idx = 90
+    target_var = 'fch4'
+    forcing_group = 'temperature'  # select variable groups that are included for fch4 prediction. Three options: 'temperature','temperature_gpp','temperature_gpp_water'
+    dir_forcing = './data/forcing_v2/'  # path of input forcing datasets
+    dir_fch4 = './results/wetland_FCH4_2002-2021_upscale_result.nc'  # path of upscaled methane dataset
+    save_file = f'./results/wetland_FCH4_partial_corr.nc'  # path to save the partial correlation results
+
+    forcing_file_list = ['soil_temperature_level_1', '2m_temperature', 'snow_cover', 'surface_pressure',
+                         'total_precipitation', 'volumetric_soil_water_layer_1',
+                         'windspeed']  # the file names of input forcing
+    forcing_vars = ['stl1', 't2m', 'snowc', 'sp',
+                    'tp', 'swvl1', 'ws']  # the names of the input forcing
+
+    #Calculate partial correlation between FCH4 and its drivers in each grid cell
+    grid_variation_analysis(max_lat_idx,dir_forcing,dir_fch4,save_file,forcing_file_list,forcing_vars,target_var)
+    #Calculate FCH4 driven by different variables using statistical linear regression models
+    grid_trend_analysis(forcing_group, max_lat_idx, dir_forcing, dir_fch4, forcing_file_list, forcing_vars)
+
+
+
+
+
+
---
+++ BorealArctic/Analysis/methane_analysis_toolbox.py
@@ -0,0 +1,127 @@
+import pandas as pd
+import numpy as np
+from netCDF4 import Dataset
+from scipy import signal
+import pingouin as pg
+import statsmodels.api as sm
+
+def FCH4Data_load(dir_fch4,max_lat_idx):#load upscaled FCH4 dataset
+    nc = Dataset(dir_fch4, 'r')
+    fch4_var = nc.variables['FCH4_enemble_mean'][-1, 52:, :max_lat_idx].astype(np.float32)
+    lats = nc.variables['latitude'][:max_lat_idx]
+    lons = nc.variables['longitude'][:]
+    nc.close()
+    return fch4_var,lats,lons
+
+def FCH4_anomaly_calculation(fch4_var,lats,lons):#calculate FCH4 anomaly
+    fch4_var = fch4_var.reshape(-1, 52, len(lats), len(lons))
+    fch4_var_mean = np.nanmean(fch4_var, axis=0, keepdims=True)
+    fch4_var_anomaly = fch4_var - fch4_var_mean
+    fch4_var_anomaly = fch4_var_anomaly.reshape(-1, len(lats), len(lons))
+    return fch4_var_anomaly
+
+def FCH4_anomaly_yearly(fch4_var,lats,lons):#calculate FCH4 anomaly in yearly scale
+    fch4_var = fch4_var.reshape(-1, 52, len(lats), len(lons))
+    fch4_var = np.nanmean(fch4_var, axis=1)
+    fch4_var_mean = np.nanmean(fch4_var, axis=0, keepdims=True)
+    fch4_var_anomaly = fch4_var - fch4_var_mean
+    return fch4_var_anomaly
+
+def ForcingData_load(forcing_file_list,forcing_vars,dir_forcing,max_lat_idx,lats,lons):#obtain forcing datasets including TS, TA, SC, PA, P, SWC, WS, and GPP
+    start = True
+    for file in forcing_file_list:# load each nc file of different input forcing
+        forcing_var = forcing_vars[forcing_file_list.index(file)]
+        nc_file = dir_forcing + f'ecmwf_weekly_0.5degree_unified_2000-2021_{file}.nc'
+        nc = Dataset(nc_file, 'r')
+        temp_var = nc.variables[forcing_var][52 * 2:, :max_lat_idx].astype(np.float32)  # (start from the year of 2002)
+        nc.close()
+        temp_var = temp_var[np.newaxis]
+        if start:
+            all_forcing_vars = temp_var
+            start = False
+        else:
+            all_forcing_vars = np.concatenate((all_forcing_vars, temp_var), axis=0)
+        print('loading', file)
+    # obatain datsets of GPP
+    nc_file = dir_forcing + f'GOSIF_GPP_05degree_weekly_2001-2021_v2.nc'
+    nc = Dataset(nc_file, 'r')
+    temp_var = nc.variables['GPP'][52:, :max_lat_idx].astype(np.float32)# (start from the year of 2002)
+    nc.close()
+    temp_var = temp_var[np.newaxis]
+    # combine GPP with other variables
+    all_forcing_vars = np.concatenate((all_forcing_vars, temp_var), axis=0)
+    all_forcing_vars = all_forcing_vars.reshape(all_forcing_vars.shape[0], -1, 52, len(lats), len(lons))
+    return all_forcing_vars
+
+def Forcing_anomaly_calculation(all_forcing_vars,lats,lons):#calculate forcing anomaly, including TS, TA, SC, PA, P, SWC, WS, and GPP
+    all_forcing_vars_mean = np.nanmean(all_forcing_vars, axis=1, keepdims=True)
+    all_forcing_vars_anomaly = all_forcing_vars - all_forcing_vars_mean
+    all_forcing_vars_anomaly = all_forcing_vars_anomaly.reshape(all_forcing_vars.shape[0], -1, len(lats), len(lons))
+    return all_forcing_vars_anomaly
+
+def Forcing_anomaly_yearly(all_forcing_vars,lats,lons):#calculate forcing anomaly in yearly scale, including TS, TA, SC, PA, P, SWC, WS, and GPP
+    all_forcing_vars = np.nanmean(all_forcing_vars, axis=2)
+    all_forcing_vars_mean = np.nanmean(all_forcing_vars, axis=1, keepdims=True)
+    all_forcing_vars_anomaly = all_forcing_vars - all_forcing_vars_mean
+    return all_forcing_vars_anomaly
+
+def Par_cor(forcing_vars,lats,lons,all_forcing_vars_anomaly,fch4_var_anomaly,all_vars,target_var): #calculate partial correlation between FCH4 and its drivers
+    partial_result = np.full((len(forcing_vars), len(lats), len(lons)), np.nan)
+    for lat_idx in range(len(lats)):  # len(lats)
+        for lon_idx in range(len(lons)):  # len(lons)
+            print(lat_idx, lon_idx)
+            if np.sum(np.isnan(all_forcing_vars_anomaly[:, :, lat_idx, lon_idx].reshape(-1))) == 0:
+                if np.sum(np.isnan(fch4_var_anomaly[:, lat_idx, lon_idx])) == 0:
+                    input_series = all_forcing_vars_anomaly[:, :, lat_idx, lon_idx]
+                    target_series = fch4_var_anomaly[:, lat_idx, lon_idx]
+                    target_series = target_series[np.newaxis]
+                    data = np.concatenate((input_series, target_series), axis=0)
+                    data = data.transpose((1, 0))
+                    data = signal.detrend(data, axis=0)
+                    if np.sum(np.isnan(data.reshape(-1))) == 0:
+                        df = pd.DataFrame(data, columns=all_vars)
+                        for temp_var in forcing_vars:
+                            source_var = temp_var
+                            condition_vars = list(set(forcing_vars).difference(set([source_var])))
+                            res = pg.partial_corr(data=df, x=source_var, y=target_var, covar=condition_vars).round(3)
+                            r = res[['r', 'p-val']].values
+                            r, p = r[0, 0], r[0, 1]
+                            partial_result[forcing_vars.index(temp_var), lat_idx, lon_idx] = r if p < 0.05 else np.nan
+    return partial_result
+
+def LinearReg_model(forcing_vars,lats,lons,fch4_var_anomaly,all_forcing_vars_anomaly,all_vars,forcing_group):#build linear regression models to calculate FCH4 driven by different variables in each grid cell
+    corr_result = np.full((len(forcing_vars), len(lats), len(lons)), np.nan)
+    prediction_result = np.full((fch4_var_anomaly.shape[0], len(lats), len(lons)), np.nan)
+    # calculate the predicted FCH4 in each grid
+    for lat_idx in range(len(lats)):
+        for lon_idx in range(len(lons)):
+            print(lat_idx, lon_idx)
+            if np.sum(np.isnan(all_forcing_vars_anomaly[:, :, lat_idx, lon_idx].reshape(-1))) == 0:
+                input_series = all_forcing_vars_anomaly[:, :, lat_idx, lon_idx]
+                target_series = fch4_var_anomaly[:, lat_idx, lon_idx]
+                target_series = target_series[np.newaxis]
+                data = np.concatenate((input_series, target_series), axis=0)
+                data = data.transpose((1, 0))
+                # data = signal.detrend(data, axis=0)
+                if np.sum(np.isnan(data.reshape(-1))) == 0:
+                    df = pd.DataFrame(data, columns=all_vars)
+                    if forcing_group == 'temperature':
+                        input_cols = ['stl1', 't2m']
+                    elif forcing_group == 'temperature_gpp':
+                        input_cols = ['stl1', 't2m', 'gpp']
+                    else:
+                        input_cols = ['stl1', 't2m', 'gpp', 'tp', 'swvl1']
+                    x = df[input_cols]
+                    y = df['fch4']
+                    x = sm.add_constant(x)
+
+                    model = sm.OLS(y, x).fit()
+                    predictions = model.predict(x)
+                    f_pvalue = model.f_pvalue
+                    if f_pvalue < 0.1:
+                        prediction_result[:, lat_idx, lon_idx] = predictions
+                    for i in input_cols:
+                        p = model.pvalues[i]
+                        r = model.params[i]
+                        corr_result[input_cols.index(i), lat_idx, lon_idx] = r if p < 0.1 else np.nan
+    return corr_result,prediction_result
